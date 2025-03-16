05bff30c04b0c148babfced8be2c27bb08bc5c69
Javier Rojas <javroj95@gmail.com>
2025-03-16 05:01:11 -0500

    fixed styling issue on navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -37,17 +37,16 @@ export default function NavBar({ isDark, setIsDark, cartItems, inventoryData }){
 
     return(
         <div className="bg-navBar">
-            <nav className='flex w-full items-center justify-between'>
+            <nav className='flex items-center justify-between'>
                 <Link className='contents' to='/'><img className='size-20 scale-90 rounded-full m-1 ml-8 hover:scale-100' src={profPic} alt="profPic"/> </Link>
                 <SearchBar inventoryData={inventoryData} />
                 <ul className='flex'>
                     <button onClick={() => setIsDark(!isDark)} className=' origin-right scale-90 hover:scale-100 size-16 pt-3'>
                     <img src = {isDark ? sun : moon} />
                     </button>                    
-                    <li className="flex">
+                    <li className="grid grid-cols-2 ">
                        <NavLink className={({ isActive }) => ["m-3", "p-3","rounded-xl","border-textColor","bg-none", "scale-90", "text-lg/6", "text-textColor", "hover:scale-100", "font-mono", ,isActive ? "border" : ""].join(" ")} to='/about'> About </NavLink> 
                        <NavLink className={({ isActive }) => ["border-textColor", "rounded-xl", "bg-none", "m-2", "scale-90", "px-4", "py-3", "hover:scale-100", isActive ? "border" : ""].join(" ")} to='/cart'> <img src={cartImage} alt="cartImg" className='size-10'/> </NavLink>  
-
                     </li>
                 </ul>
             </nav>
